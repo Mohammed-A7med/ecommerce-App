e6474e97a403ca29d3f5d60d7da0ebd18469c177
Mohammed-A7med <mohamedahmedkhalaf68@gmail.com>
2025-12-21 15:43:01 +0200

    feat: add products data fetching with error handling

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,30 @@ import { Poppins } from "next/font/google";
 import HomeCategories from "@/components/HomeCategories/HomeCategories";
 import Section from "@/components/Section/Section";
 import MainSlider from "@/components/Sliders/MainSlider";
+import { Product, ProductData } from "@/types/product";
 
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "600", "700"],
 });
 
-export default function Home() {
+async function getProducts(): Promise<Product[]> {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/products?limit=8`,
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const data: ProductData = await res.json();
+  return data.data;
+}
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
+  
   return (
     <>
       <MainSlider />
